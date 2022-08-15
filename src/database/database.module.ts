913e409191804e19edf2bfc689a59e2d7c2d3d23
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2022-08-14 22:43:22 -0300

    fix: Fixing imports

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -32,7 +32,7 @@ import { IsConvenioActiveConstraint } from '../validators/isConvenioActive.valid
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USER,
       password: `${process.env.DB_PASS}`,
